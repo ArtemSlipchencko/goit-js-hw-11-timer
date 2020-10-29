642e13ce7e6e90af6375d0ebf1d21f33649d8ee7
ArtemSlipchencko <68220186+ArtemSlipchencko@users.noreply.github.com>
2020-10-29 17:17:16 +0200

    исправления

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
-const Container = document.querySelector('#timer-1');
-const Days = document.querySelector('[data-value="days"]');
-const Hours = document.querySelector('[data-value="hours"]');
-const Mins = document.querySelector('[data-value="mins"]');
-const Secs = document.querySelector('[data-value="secs"]');  
+const body = document.querySelector('body');
 
 class CountdownTimer {
 
     constructor({selector, targetDate}) {
         this.selector = selector;
         this.targetDate = targetDate;
+        this.refs = this.getRefs();
     };
 
     run = function() {
+
+        
 
         setInterval(() => {
         const time = this.targetDate - Date.now(); 
@@ -20,16 +19,50 @@ class CountdownTimer {
 
     }
 
+    getRefs = function() {
+
+        body.innerHTML += `<div class="timer" id="${this.selector}">
+        <div class="field">
+          <span class="value" data-value="days">11</span>
+          <span class="label">Days</span>
+        </div>
+      
+        <div class="field">
+          <span class="value" data-value="hours">11</span>
+          <span class="label">Hours</span>
+        </div>
+      
+        <div class="field">
+          <span class="value" data-value="mins">11</span>
+          <span class="label">Minutes</span>
+        </div>
+      
+        <div class="field">
+          <span class="value" data-value="secs">11</span>
+          <span class="label">Seconds</span>
+        </div>
+      </div>`
+
+        const container = document.querySelector(`#${this.selector}`);
+        const days = container.querySelector('[data-value="days"]');
+        const hours = container.querySelector('[data-value="hours"]');
+        const mins = container.querySelector('[data-value="mins"]');
+        const secs = container.querySelector('[data-value="secs"]'); 
+        return {days, hours, mins, secs};
+
+    }
+
     getFormat = function(str) {
         return String(str).padStart(2, '0');
-        }
+    }
 
     refresh = function(time) {
 
-        Days.textContent = this.getFormat(Math.floor(time / (1000 * 60 * 60 * 24)));
-        Hours.textContent = this.getFormat(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        Mins.textContent = this.getFormat(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        Secs.textContent = this.getFormat(Math.floor((time % (1000 * 60)) / 1000));  
+        const {days, hours, mins, secs} = this.refs;
+        days.textContent = this.getFormat(Math.floor(time / (1000 * 60 * 60 * 24)));
+        hours.textContent = this.getFormat(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        mins.textContent = this.getFormat(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        secs.textContent = this.getFormat(Math.floor((time % (1000 * 60)) / 1000));  
 
     }
 
@@ -39,7 +72,7 @@ class CountdownTimer {
 };
 
 const timer  = new CountdownTimer({
-    selector: '#timer-1',
+    selector: 'timer-1',
     targetDate: new Date('November 12, 2020, 00:00:01'),
 });
 
